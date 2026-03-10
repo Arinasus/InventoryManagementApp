d668c951e781f5967de43d92eeebab49bfa21be1
Arinana <allyut12zk@gmail.com>
2026-03-10 17:48:10 +0300

    Изменение окружения

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
-namespace InventoryManagerApp.Controllers
+namespace InventoryManagementApp.Controllers
 {
     public class HomeController : Controller
     {
--- Controllers/InventoryController.cs
+++ Controllers/InventoryController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace InventoryManagerApp.Controllers
+namespace InventoryManagementApp.Controllers
 {
     [Authorize]
     public class InventoryController : Controller

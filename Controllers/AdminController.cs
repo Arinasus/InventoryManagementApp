55e98b052ff0b7c92264af88d6222262d8a16441
Arinana <allyut12zk@gmail.com>
2026-03-10 11:24:45 +0300

    Добавление администратора

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,12 +1,55 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using InventoryManagementApp.Model;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace InventoryManagementApp.Controllers
 {
+    [Authorize(Roles="Admin")]
     public class AdminController : Controller
     {
-        public IActionResult Index()
+        private readonly UserManager<ApplicationUser> _userManager;
+        public AdminController(UserManager<ApplicationUser> userManager)
         {
-            return View();
+            _userManager = userManager;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var users = await _userManager.Users.ToListAsync();
+            return View(users);
+        }
+        public async Task<IActionResult> MakeAdmin(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            await _userManager.AddToRoleAsync(user, "Admin");
+            return RedirectToAction("Index"); 
+        }
+        public async Task<IActionResult> RemoveAdmin(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            await _userManager.RemoveFromRoleAsync(user, "Admin");
+            return RedirectToAction("Index");
+        }
+        public async Task<IActionResult> Block(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            user.IsBlocked = true;
+            await _userManager.UpdateAsync(user);
+            return RedirectToAction("Index");
+        }
+        public async Task<IActionResult> Unblock(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            user.IsBlocked = false;
+            await _userManager.UpdateAsync(user);
+            return RedirectToAction("Index");
+        }
+        public async Task<IActionResult> Delete(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            await _userManager.DeleteAsync(user);
+            return RedirectToAction("Index");
         }
     }
 }

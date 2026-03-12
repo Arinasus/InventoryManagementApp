879c3e3ed9c236f1d58e206ee2f1c4669bcf65dd
Arinana <allyut12zk@gmail.com>
2026-03-12 15:27:25 +0300

    Допущение пустого описания

--- Models/InventorySettingsViewModel.cs
+++ Models/InventorySettingsViewModel.cs
@@ -4,7 +4,7 @@
     {
         public int InventoryId { get; set; }
         public string Title { get; set; }
-        public string Description { get; set; }
+        public string Description { get; set; } = "";
         public string Category { get; set; }
 
         public bool IsPublic { get; set; }

2e347c5e3f50a7506fdfc9180a245523c8103401
Arinana <allyut12zk@gmail.com>
2026-03-16 00:59:14 +0300

    добавление поиска

---
+++ Controllers/SearchController.cs
@@ -0,0 +1,45 @@
+﻿using InventoryManagementApp.Data;
+using InventoryManagementApp.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryManagementApp.Controllers
+{
+    [AllowAnonymous]
+    public class SearchController : Controller
+    {
+
+            private readonly AppDbContext _context;
+
+            public SearchController(AppDbContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<IActionResult> Index(string q)
+            {
+                if (string.IsNullOrWhiteSpace(q))
+                    return View(new SearchResultsViewModel());
+
+                var books = await _context.Inventories
+                    .Where(i => EF.Functions.ToTsVector("russian", i.Title + " " + i.Description)
+                        .Matches(EF.Functions.PlainToTsQuery("russian", q)))
+                    .ToListAsync();
+
+                var items = await _context.InventoryItems
+                    .Include(i => i.FieldValues)
+                    .Where(i => EF.Functions.ToTsVector("russian", i.CustomId)
+                        .Matches(EF.Functions.PlainToTsQuery("russian", q)))
+                    .ToListAsync();
+
+                return View(new SearchResultsViewModel
+                {
+                    Query = q,
+                    Books = books,
+                    Items = items
+                });
+            }
+        }
+}
---
+++ Models/SearchResultsViewModel.cs
@@ -0,0 +1,11 @@
+﻿using InventoryManagementApp.Model;
+
+namespace InventoryManagementApp.Models
+{
+    public class SearchResultsViewModel
+    {
+        public string Query { get; set; }
+        public List<Inventory> Books { get; set; } = new();
+        public List<InventoryItem> Items { get; set; } = new();
+    }
+}

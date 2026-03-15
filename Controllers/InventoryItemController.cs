9c07b2e8ce641888ba3a16b5468cd9d0d37c748d
Arinana <allyut12zk@gmail.com>
2026-03-15 23:50:53 +0300

    Добавление лайков

---
+++ Controllers/InventoryItemController.cs
@@ -0,0 +1,50 @@
+﻿using InventoryManagementApp.Data;
+using InventoryManagementApp.Model;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryManagementApp.Controllers
+{
+    public class InventoryItemController : Controller
+    {
+        private readonly AppDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+        public InventoryItemController(AppDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+        [HttpPost]
+        public async Task<IActionResult> ToggleLike(int itemId)
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+                return Unauthorized();
+
+            var like = await _context.ItemLikes
+                .FirstOrDefaultAsync(l => l.ItemId == itemId && l.UserId == user.Id);
+
+            if (like == null)
+            {
+                _context.ItemLikes.Add(new ItemLike
+                {
+                    ItemId = itemId,
+                    UserId = user.Id,
+                    LikedAt = DateTime.UtcNow
+                });
+            }
+            else
+            {
+                _context.ItemLikes.Remove(like);
+            }
+
+            await _context.SaveChangesAsync();
+
+            int count = await _context.ItemLikes.CountAsync(l => l.ItemId == itemId);
+
+            return Json(new { likes = count });
+        }
+
+    }
+}

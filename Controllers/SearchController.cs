95c7e31fafe6335c65e7f0c73e279d1cda05a374
Arinana <allyut12zk@gmail.com>
2026-03-16 03:06:04 +0300

    Добавление ссылок

--- Controllers/SearchController.cs
+++ Controllers/SearchController.cs
@@ -1,4 +1,5 @@
 ﻿using InventoryManagementApp.Data;
+using InventoryManagementApp.Model;
 using InventoryManagementApp.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
@@ -18,28 +19,43 @@ namespace InventoryManagementApp.Controllers
                 _context = context;
             }
 
-            public async Task<IActionResult> Index(string q)
+        public async Task<IActionResult> Index(string q, string tag)
+        {
+            if (!string.IsNullOrWhiteSpace(tag))
             {
-                if (string.IsNullOrWhiteSpace(q))
-                    return View(new SearchResultsViewModel());
-
-                var books = await _context.Inventories
-                    .Where(i => EF.Functions.ToTsVector("russian", i.Title + " " + i.Description)
-                        .Matches(EF.Functions.PlainToTsQuery("russian", q)))
-                    .ToListAsync();
-
-                var items = await _context.InventoryItems
-                    .Include(i => i.FieldValues)
-                    .Where(i => EF.Functions.ToTsVector("russian", i.CustomId)
-                        .Matches(EF.Functions.PlainToTsQuery("russian", q)))
+                var booksByTag = await _context.Inventories
+                    .Include(i => i.Tags)
+                    .Where(i => i.Tags.Any(t => t.Name == tag))
                     .ToListAsync();
 
                 return View(new SearchResultsViewModel
                 {
-                    Query = q,
-                    Books = books,
-                    Items = items
+                    Query = tag,
+                    Books = booksByTag,
+                    Items = new List<InventoryItem>()
                 });
             }
+
+            if (string.IsNullOrWhiteSpace(q))
+                return View(new SearchResultsViewModel());
+
+            var books = await _context.Inventories
+                .Where(i => EF.Functions.ToTsVector("russian", i.Title + " " + i.Description)
+                    .Matches(EF.Functions.PlainToTsQuery("russian", q)))
+                .ToListAsync();
+
+            var items = await _context.InventoryItems
+                .Include(i => i.FieldValues)
+                .Where(i => EF.Functions.ToTsVector("russian", i.CustomId)
+                    .Matches(EF.Functions.PlainToTsQuery("russian", q)))
+                .ToListAsync();
+
+            return View(new SearchResultsViewModel
+            {
+                Query = q,
+                Books = books,
+                Items = items
+            });
         }
+    }
 }
